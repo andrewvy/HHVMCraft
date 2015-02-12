0a9ace1bb19722e17314f2970b3cda7556e635ee
Andrew Vy <andrew@andrewvy.com>
2015-02-11 17:06:52 -0800

    Add temporary hex dump function

--- HHVMCraft.Core/Networking/MultiplayerServer.php
+++ HHVMCraft.Core/Networking/MultiplayerServer.php
@@ -29,16 +29,45 @@ class MultiplayerServer extends EventEmitter {
 
 	public function start($port) {
 		$this->socket->on('connection', function($client) {
-			echo " >> New Connection";
+			echo " >> New Connection \n";
 			
-			$this->acceptClient($client);
-			socket_write($client->stream,"Hello world!");
-			$client->close();
+			$this->acceptClient($client);	
+
+			socket_write($client->stream, 0x022D);
+			$client->on('data', function($data) use ($client) {
+				$this->hex_dump($data);
+			});
 		});
 		
 		$this->socket->listen($port);
 		$this->loop->run();
 		
-		echo " >> Listening on address: " + $this->address + ":" + $port;
+		echo " >> Listening on address: " + $this->address + ":" + $port + "\n";
 	}
+
+	public function hex_dump($data, $newline="\n") {
+		static $from = '';
+		static $to = '';
+
+		static $width = 16; # number of bytes per line
+
+		static $pad = '.'; # padding for non-visible characters
+
+		if ($from==='') {
+			for ($i=0; $i<=0xFF; $i++) {
+				$from .= chr($i);
+				$to .= ($i >= 0x20 && $i <= 0x7E) ? chr($i) : $pad;
+			}
+		}
+
+		$hex = str_split(bin2hex($data), $width*2);
+		$chars = str_split(strtr($data, $from, $to), $width);
+
+		$offset = 0;
+		foreach ($hex as $i => $line) {
+			echo sprintf('%6X',$offset).' : '.implode(' ', str_split($line,2)) . ' [' . $chars[$i] . ']' . $newline;
+			$offset += $width;
+		}
+	}
+
 }
--- server.php
+++ server.php
@@ -3,7 +3,7 @@
 require "HHVMCraft.Core/Networking/MultiplayerServer.php";
 use HHVMCraft\Core\Networking\MultiplayerServer;
 
-echo " >> Starting Server";
+echo " >> Starting Server \n";
 
 $addr = "127.0.0.01";
 $server = new MultiplayerServer($addr);
