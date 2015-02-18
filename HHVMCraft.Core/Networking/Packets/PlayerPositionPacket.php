2bc0ed62192004f2d3abe800fbbc7837b9742549
Andrew Vy <andrew@andrewvy.com>
2015-02-18 08:24:10 -0800

    Forgot how to syntax!

--- HHVMCraft.Core/Networking/Packets/PlayerPositionPacket.php
+++ HHVMCraft.Core/Networking/Packets/PlayerPositionPacket.php
@@ -6,17 +6,17 @@ class PlayerPositionPacket {
 	const id = "0b";
 
 	public $x;
-	public $y
+	public $y;
 	public $stance;
 	public $z;
 	public $onGround;
 
 	public function readPacket($StreamWrapper) {
-		$this->x = $StreamWrapper->readDouble()
-		$this->y = $StreamWrapper->readDouble()
-		$this->stance = $StreamWrapper->readDouble()
-		$this->z = $StreamWrapper->readDouble()
-		$this->onGround = $StreamWrapper->readBool()
+		$this->x = $StreamWrapper->readDouble();
+		$this->y = $StreamWrapper->readDouble();
+		$this->stance = $StreamWrapper->readDouble();
+		$this->z = $StreamWrapper->readDouble();
+		$this->onGround = $StreamWrapper->readBool();
 	}
 
 }
