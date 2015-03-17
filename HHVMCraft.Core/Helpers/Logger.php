7f53384b0a3ad187461e53f65d67f6cf3144f68d
Andrew Vy <andrew@andrewvy.com>
2015-03-16 17:17:35 -0700

    super simple logger

---
+++ HHVMCraft.Core/Helpers/Logger.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace HHVMCraft\Core\Helpers;
+
+class Logger {
+	const PREFIX = " HHVMCraft >> ";
+	const ERROR_PREFIX = "[ERROR]";
+	const WARNING_PREFIX = "[WARNING]";
+	const LOG_PREFIX = "[LOG]";
+
+	public $options;
+
+	public function __construct($options) {
+		$this->options = $options;
+	}
+
+	public function throwLog($msg) {
+		echo $this::LOG_PREFIX.$this::PREFIX.$msg;
+	}
+
+	public function throwWarning($msg) {
+		echo $this::WARNING_PREFIX.$this::PREFIX.$msg;
+	}
+
+	public function throwError($msg) {
+		echo $this::ERROR_PREFIX.$this::PREFIX.$msg;
+	}
+}
