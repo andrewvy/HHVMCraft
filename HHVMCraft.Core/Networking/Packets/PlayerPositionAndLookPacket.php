6bc4141351e6d9c1b2b48ce187f7ea5c1f91ba63
Andrew Vy <andrew@andrewvy.com>
2015-04-13 23:18:49 -0700

    Fix PositionAndLookPacket from not reading double

--- HHVMCraft.Core/Networking/Packets/PlayerPositionAndLookPacket.php
+++ HHVMCraft.Core/Networking/Packets/PlayerPositionAndLookPacket.php
@@ -17,13 +17,12 @@ class PlayerPositionAndLookPacket {
 	public $onGround;
 
 	public function readPacket($StreamWrapper) {
-		$StreamWrapper->readDouble();
+		$this->x = $StreamWrapper->readDouble();
 		$this->y = $StreamWrapper->readDouble();
 		$this->stance = $StreamWrapper->readDouble();
 		$this->z = $StreamWrapper->readDouble();
 		$this->yaw = $StreamWrapper->readInt();
 		$this->pitch = $StreamWrapper->readInt();
 		$this->onGround = $StreamWrapper->readBool();
-		//		echo " Position: <".$this->x.",".$this->y.",".$this->z."> Yaw: ".$this->yaw." , Pitch: ".$this->pitch."\n";
 	}
 }
