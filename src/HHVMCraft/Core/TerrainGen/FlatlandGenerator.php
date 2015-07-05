10e099d62dd8f049daaed84970e6269838c19d54
Andrew Vy <andrew@andrewvy.com>
2015-07-04 20:05:56 -0700

    Retab terrain generator

--- src/HHVMCraft/Core/TerrainGen/FlatlandGenerator.php
+++ src/HHVMCraft/Core/TerrainGen/FlatlandGenerator.php
@@ -13,33 +13,32 @@ use HHVMCraft\API\Coordinates3D;
 use HHVMCraft\Core\World\Chunk;
 
 class FlatlandGenerator {
-  const LevelType = "FLAT";
-  public $spawnpoint;
-  public $layers = [];
+	const LevelType = "FLAT";
+	public $spawnpoint;
+	public $layers = [];
 
-  public function __construct() {
-    $this->spawnpoint = new Coordinates3D(0, 10, 0);
-  }
+	public function __construct() {
+		$this->spawnpoint = new Coordinates3D(0, 10, 0);
+	}
 
-  public function generateChunk($Coordinates2DPos) {
-    $newC = new Chunk($Coordinates2DPos);
-    $y = 0;
-    // Flatland, dirt from layer 1 - 8, grass on layer 9.
-    while ($y < 10) {
-      for ($x = 0; $x < 16; $x++) {
-        for ($z = 0; $z < 16; $z++) {
-          if ($y < 9) {
-            $newC->setBlockID(new Coordinates3D($x, $y, $z), 0x03);
-          }
-          else {
-            $newC->setBlockID(new Coordinates3D($x, $y, $z), 0x02);
-          }
-        }
-      }
+	public function generateChunk($Coordinates2DPos) {
+		$newC = new Chunk($Coordinates2DPos);
+		$y = 0;
+		// Flatland, dirt from layer 1 - 8, grass on layer 9.
+		while ($y < 10) {
+			for ($x = 0; $x < 16; $x++) {
+				for ($z = 0; $z < 16; $z++) {
+					if ($y < 9) {
+						$newC->setBlockID(new Coordinates3D($x, $y, $z), 0x03);
+					} else {
+						$newC->setBlockID(new Coordinates3D($x, $y, $z), 0x02);
+					}
+				}
+			}
 
-      $y++;
-    }
+			$y++;
+		}
 
-    return $newC;
-  }
+		return $newC;
+	}
 }
