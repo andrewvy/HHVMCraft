3c00baa72028c1ff527db1dfe25306b5e55c0b3f
Andrew Vy <andrew@andrewvy.com>
2015-08-21 17:59:29 -0700

    Add WorldTest

---
+++ tests/Core/World/ChunkTest.php
@@ -0,0 +1,16 @@
+<?php
+
+use HHVMCraft\Core\World\Chunk;
+use HHVMCraft\API\Coordinates3D;
+use HHVMCraft\API\Coordinates2D;
+
+class ChunkTest extends PHPUnit_Framework_TestCase {
+	public function testChunkGeneration() {
+		$chunkCoordinates = new Coordinates2D(0,0);
+		$blockCoordinates = new Coordinates3D(10,10,10);
+		$chunk = new Chunk($chunkCoordinates);
+
+		$this->assertEquals($chunk->getBlockID($blockCoordinates), 0x00, "Expects block to be air in the chunk");
+	}
+}
+
--- tests/Core/World/WorldTest.php
+++ tests/Core/World/WorldTest.php
@@ -1,14 +1,22 @@
 <?php
 
-use HHVMCraft\Core\World\Chunk;
+use HHVMCraft\Core\World\World;
+use HHVMCraft\API\BlockProvider;
 use HHVMCraft\API\Coordinates3D;
 use HHVMCraft\API\Coordinates2D;
 
 class WorldTest extends PHPUnit_Framework_TestCase {
-	public function testChunkGeneration() {
+	public function testWorldGeneration() {
 		$chunkCoordinates = new Coordinates2D(0,0);
-		$blockCoordinates = new Coordinates3D(10,10,10);
-		$chunk = new Chunk($chunkCoordinates);
+		$blockCoordinates = new Coordinates3D(0,20,0);
+
+		$BlockProvider = new BlockProvider();
+
+		$world = new World("testworld", $BlockProvider);
+
+		$this->assertEquals($world->worldname, "testworld", "Expects world name to be testworld");
+
+		$chunk = $world->generateChunk($chunkCoordinates);
 
 		$this->assertEquals($chunk->getBlockID($blockCoordinates), 0x00, "Expects block to be air in the chunk");
 	}
