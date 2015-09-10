06402b8729ed97ac6b3e3bd0efae0f2a99c95e95
Andrew Vy <andrew@andrewvy.com>
2015-09-10 08:29:57 -0700

    Scaffolding for Client tests

---
+++ tests/Core/Networking/ClientTest.php
@@ -0,0 +1,27 @@
+<?php
+
+use Evenement\EventEmitter;
+use HHVMCraft\Core\Networking\MultiplayerServer;
+use HHVMCraft\Core\Networking\Client;
+/*
+
+class FakeConnection extends EventEmitter {
+}
+
+class FakeServer {
+	public $CraftingRepository;
+
+	public function handlePacket($client) {
+		return true;
+	}
+}
+
+class ClientTest extends PHPUnit_Framework_TestCase {
+	public function testClientCreation() {
+		$server = new MultiplayerServer(25565);
+		$connection = new FakeConnection();
+		$client = new Client($connection, $server);
+	}
+}
+
+*/
