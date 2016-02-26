1ec44ffc6c61e0e67bf7c986ab7a71dfb8705fb5
Lance Pioch <lance@lancepioch.com>
2016-02-26 16:11:41 -0500

    Remove the license header and fix #30

--- src/HHVMCraft/Core/Entities/LivingEntity.php
+++ src/HHVMCraft/Core/Entities/LivingEntity.php
@@ -1,10 +1,5 @@
 <?php
-/**
- * This file is part of HHVMCraft - a Minecraft server implemented in PHP
- *
- * @copyright Andrew Vy 2015
- * @license MIT <https://github.com/andrewvy/HHVMCraft/blob/master/LICENSE.md>
- */
+
 namespace HHVMCraft\Core\Entities;
 
 class LivingEntity extends Entity {
--- src/HHVMCraft/Core/Networking/Handlers/ChatHandler.php
+++ src/HHVMCraft/Core/Networking/Handlers/ChatHandler.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * This file is part of HHVMCraft - a Minecraft server implemented in PHP
- *
- * @copyright Andrew Vy 2015
- * @license MIT <https://github.com/andrewvy/HHVMCraft/blob/master/LICENSE.md>
- */
 
 namespace HHVMCraft\Core\Networking\Handlers;
 
